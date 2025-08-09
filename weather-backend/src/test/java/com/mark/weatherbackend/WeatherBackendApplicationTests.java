38ec401b94a2e0565941a5edbf65c022d3ab3083
Guodong Liu <guodongl@usc.edu>
2025-08-09 18:26:36 -0400

    Initial commit - backend part

---
+++ weather-backend/src/test/java/com/mark/weatherbackend/WeatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.weatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
